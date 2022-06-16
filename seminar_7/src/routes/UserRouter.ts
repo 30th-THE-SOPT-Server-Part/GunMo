9f0c236aa049640eca9a3882ca1b92156e040e55
gunom <gu3062@naver.com>
2022-06-16 23:18:43 +0900

    [Add]: seminar7 level1

---
+++ seminar_7/src/routes/UserRouter.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { body } from "express-validator";
+import { UserController } from "../controllers";
+import User from "../models/User";
+
+const router: Router = Router();
+
+// route => use (/user) => post (/)
+router.post('/', [
+    body('email').isEmail(),
+    body('password').isLength({min:6}),
+    body('name').notEmpty(),
+    body('phone').notEmpty()
+], UserController.createUser);
+router.put('/:userId', UserController.updateUser);
+router.get('/:userId', UserController.findUserById);
+router.delete('/:userId', UserController.deleteUser);
+router.post('/signin', [
+    body('email').notEmpty(),
+    body('email').isEmail(),
+    body('password').isLength({min:6}),
+    body('password').notEmpty()
+], UserController.signInUser);
+
+export default router;
